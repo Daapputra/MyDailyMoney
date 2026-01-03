02c1b8ef1cbc6fc54b4f681fccf3e28dfa87c817
Lenovo <daffanaufal0606@gmail.com>
2026-01-03 15:10:32 +0700

    final projek

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.mydailymoney"
+        applicationId = "com.uas.mydailymoney"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
